14627d97cfea1e029b586fc34a7a0b738e1f2d74
Juraj Žec <juro.zec.1@gmail.com>
2022-02-10 09:10:03 +0100

    snackbar

--- src/contexts/SnackbarContext.tsx
+++ src/contexts/SnackbarContext.tsx
@@ -1,15 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import { getDatabase, ref, onValue } from 'firebase/database';
+import {
+  getDatabase,
+  ref,
+  onValue,
+  query,
+  limitToLast
+} from 'firebase/database';
 import {Button, Snackbar} from '@mui/material';
 import { StudentEvent } from '../types';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, useLocation } from 'react-router-dom';
 
-const SnackbarContext = React.createContext({
-  // showSnackbar: (data: StudentEvent) => null
-});
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles(() => ({
+  snackbar: {
+    bottom: 80
+  },
+}));
+
+
+const SnackbarContext = React.createContext({});
 
 export const SnackbarProvider = ({ children }) => {
+  const classes = useStyles();
   const [event, setEvent] = useState<StudentEvent>();
+  const { pathname } = useLocation();
 
   const handleClose = () => {
     setEvent(null);
@@ -17,10 +32,18 @@ export const SnackbarProvider = ({ children }) => {
 
   useEffect(() => {
     const db = getDatabase();
-    const studentEventsRef = ref(db, 'studentEvents');
-    const unsubscribe = onValue(studentEventsRef, (snapshot) => {
+    const studentQuery = query(
+      ref(db, 'studentEvents'),
+      // orderByChild('createdAt'),
+      limitToLast(1)
+    );
+
+    const unsubscribe = onValue(studentQuery, (snapshot) => {
       if (snapshot.exists()) {
-        const newEvent: StudentEvent = Object.values(snapshot.val())[0] as StudentEvent;
+        const newEvent: StudentEvent = Object.values(
+          snapshot.val()
+        )[0] as StudentEvent;
+        console.log(newEvent);
 
         setEvent({
           message: newEvent.message,
@@ -37,12 +60,20 @@ export const SnackbarProvider = ({ children }) => {
     <SnackbarContext.Provider value={{}}>
       {children}
       <Snackbar
-        open={Boolean(event)}
-        autoHideDuration={6000}
+        open={Boolean(event && pathname.startsWith('/exam/'))}
+        autoHideDuration={60000}
         onClose={handleClose}
         message={event?.message}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        className={classes.snackbar}
         action={
-          <Button color="secondary" size="small" component={RouterLink} to={event?.link || "/exam"}>
+          <Button
+            color="secondary"
+            size="small"
+            onClick={handleClose}
+            component={RouterLink}
+            to={event?.link || '/exam'}
+          >
             {event?.actionLabel}
           </Button>
         }
